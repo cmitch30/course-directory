e057cf65afb67b0970413da1f31dfc52c27a40ea
clark <clark_mitchelljr@hotmail.com>
2022-12-13 17:22:34 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Routes, Navigate } from "react-router-dom";
+import { Route, Routes, Navigate, HashRouter } from "react-router-dom";
 import { HTMLCourses, CSSCourses, JSCourses } from "../src/data/courses";
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -10,30 +10,34 @@ import Courses from "./components/Courses";
 import CourseContainer from "./components/courses/CourseContainer";
 import NotFound from "./components/NotFound";
 
-
 function App() {
   return (
-    <BrowserRouter basename="/course-directory">
-
-    <div className="container">
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-        <Route path="teachers">
-          <Route index element={<Teachers />} />
-          <Route path=":topic/:name" element={<Featured />} />
-        </Route>
-        <Route path="courses" element={<Courses />}>
-          <Route index element={<Navigate replace to="html" />} />
-          <Route path="html" element={<CourseContainer data={HTMLCourses} />} />
-          <Route path="css" element={<CourseContainer data={CSSCourses} />} />
-          <Route path="javascript" element={<CourseContainer data={JSCourses} />}/>
-        </Route>
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </div>
-    </BrowserRouter>
+    <HashRouter>
+      <div className="container">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="teachers">
+            <Route index element={<Teachers />} />
+            <Route path=":topic/:name" element={<Featured />} />
+          </Route>
+          <Route path="courses" element={<Courses />}>
+            <Route index element={<Navigate replace to="html" />} />
+            <Route
+              path="html"
+              element={<CourseContainer data={HTMLCourses} />}
+            />
+            <Route path="css" element={<CourseContainer data={CSSCourses} />} />
+            <Route
+              path="javascript"
+              element={<CourseContainer data={JSCourses} />}
+            />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </HashRouter>
   );
 }
 
